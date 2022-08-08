f8ddf38d2f77d2fcb3b9c56dee3e2eb52b807dac
Luciano Fustinoni <lucianofustinoni@hotmail.com>
2022-08-08 19:50:29 -0300

    Segunda pre-entrega

--- carrito.js
+++ carrito.js
@@ -1,110 +1,168 @@
-
 const carrito = JSON.parse(localStorage.getItem("carrito")) ?? [];
-const totalCarrito = carrito.reduce((acumulador, producto) => acumulador + producto.precio, 0)
-document.getElementById("count-cart").innerHTML = `${carrito.length} - $${totalCarrito}`;
+document.getElementById("show-cart").innerHTML = " "
+let totalCarrito = carrito.reduce((acumulador, producto) => acumulador + producto.precio, 0)
+const contador = localStorage.getItem("contador-carrito")
 document.getElementById("foot-modal").innerHTML = `
-    <div>
-        <p style="margin-bottom: 0px; font-weight:700;"> Precio total: $ ${totalCarrito}</p>
+<div>
+    <p style="margin-bottom: 0px; font-weight:700;"> Precio total: $ ${totalCarrito}</p>
+</div>
+<div>
+    <button type="button" id="delete-cart" onclick="vaciarCarrito()"><span class="text-vaciar">Vaciar carrito</span></button>
     </div>
-    <div>
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="delete-cart" onclick="vaciarCarrito()">Vaciar carrito</button>
+`
+document.getElementById("count-cart").innerHTML = 
+`
+<p>${carrito.length} - $${totalCarrito}</p> 
+`
+const mostradorCarrito = document.getElementById("show-cart")
+localStorage.getItem("mostrador-carrito")
+carrito.forEach ((prod) => {
+    mostradorCarrito.innerHTML += `<div class="cont-items">
+    <img src="${prod.imagen}" class="imagen-cart-producto">
+    <h2 class="nombre-producto-cart">${prod.nombre}</h2>
+    <p class="price-producto-cart">Precio: $${prod.precio}</p>
+    <button class="btn-eliminar-producto" id="btn-eliminar${prod.id}" onclick="eliminarDelCarrito(${prod.id})"><i class= "fas fa-trash"></i></button>
     </div>
     `
+})
 //Array de productos disponibles
 const stockDisponible = [
-    {id: 1,nombre: "Avena",precio: 180, imagen: "avena.jpg"},
-    {id: 2,nombre: "Granola",precio: 170, imagen: "granola.jpg"},
-    {id: 3,nombre: "Barritas energéticas",precio: 60, imagen: "barritas.jpg"},
-    {id: 4,nombre: "Almohaditas",precio: 120, imagen: "almohaditas.jpg"},
-    {id: 5,nombre: "Pan integral",precio: 180, imagen: "pan-integral.jpg"},
-    {id: 6,nombre: "Alfajores",precio: 100, imagen: "alfajores.jpg"},
-    {id: 7,nombre: "Leche de almendras",precio: 290, imagen: "leche.jpg"},
-    {id: 8,nombre: "Frutos secos",precio: 110, imagen: "frutos.jpg"},
-    {id: 9,nombre: "Galletitas de avena",precio: 80, imagen: "galletitas.jpg"},
-    {id: 10,nombre: "Infusiones",precio: 60, imagen: "infusiones.jpg"},
-    {id: 11,nombre: "Cereales",precio: 90, imagen: "cereales.jpg"},
-    {id: 12,nombre: "Suplemento deportivo",precio: 200, imagen: "proteinas.jpg"},
+    {id: 1,nombre: "Avena",precio: 180, imagen: "avena.jpg", cantidad:1},
+    {id: 2,nombre: "Granola",precio: 170, imagen: "granola.jpg", cantidad:1},
+    {id: 3,nombre: "Barritas energéticas",precio: 60, imagen: "barritas.jpg", cantidad:1},
+    {id: 4,nombre: "Almohaditas",precio: 120, imagen: "almohaditas.jpg", cantidad:1},
+    {id: 5,nombre: "Pan integral",precio: 180, imagen: "pan-integral.jpg", cantidad:1},
+    {id: 6,nombre: "Alfajores",precio: 100, imagen: "alfajores.jpg", cantidad:1},
+    {id: 7,nombre: "Leche de almendras",precio: 290, imagen: "leche.jpg", cantidad:1},
+    {id: 8,nombre: "Frutos secos",precio: 110, imagen: "frutos.jpg", cantidad:1},
+    {id: 9,nombre: "Galletitas de avena",precio: 80, imagen: "galletitas.jpg", cantidad:1},
+    {id: 10,nombre: "Infusiones",precio: 60, imagen: "infusiones.jpg", cantidad:1},
+    {id: 11,nombre: "Cereales",precio: 90, imagen: "cereales.jpg", cantidad:1},
+    {id: 12,nombre: "Suplemento deportivo",precio: 200, imagen: "proteinas.jpg", cantidad:1},
 ]
 
-//Creamos las cards de los productos con un forEach
+// Creamos las cards de los productos con un forEach
 stockDisponible.forEach ((producto) => {
     const botonId = `agregarCarrito${producto.id}`
     const cardsCont = document.getElementById("cards-section")
     const contenedor = document.createElement("div")
     contenedor.className = "col col-lg-3 col-dm-3 col-xs-6 contenedor-productos"
-    contenedor.innerHTML += `<div>
+    contenedor.innerHTML += `<div data-aos="fade-up" data-aos-duration="1" class="productos">
         <img src="${producto.imagen}" class="img-productos">
         <h2 class="title-productos">${producto.nombre}</h2>
-        <p class="price-productos">$ ${producto.precio}</p>
+        <p class="price-productos">Precio: $${producto.precio}</p>
         <button class="btn-agregar-carrito" id=${botonId}><span class="text-btn-add">Agregar</span></button>
     </div>`
     //Agregamos a la seccion cards-section el html creado arriba
     cardsCont.appendChild(contenedor)
 })
 
-//Por cada producto del array (metodo forEach), vamos a ejecutar una función al hacer click sobre el boton creado anteriormente
+const buscador = document.getElementById("input-buscar")
+const botonBuscador = document.getElementById("btn-buscar")
+
+
+const filtrar = () => {
+    const cardsCont = document.getElementById("cards-section")
+    const contenedor = document.createElement("div")
+    
+    
+    cardsCont.innerHTML = ''
+    let textoBuscado = buscador.value.toLowerCase();
+    stockDisponible.forEach ((producto) => {
+        const botonId = `agregarCarrito${producto.id}`
+        let nombreProductoABuscar = producto.nombre.toLowerCase();
+        if(nombreProductoABuscar.indexOf(textoBuscado) !== -1){
+            cardsCont.innerHTML += `
+                <div class= "col col-lg-3 col-dm-3 col-xs-6 contenedor-productos">
+                        <img src="${producto.imagen}" class="img-productos">
+                        <h2 class="title-productos">${producto.nombre}</h2>
+                        <p class="price-productos"> $ ${producto.precio}</p>
+                        <button class="btn-agregar-carrito" id=${botonId}><span class="text-btn-add">Agregar</span></button>
+                </div>
+            `
+            document.getElementById(botonId).addEventListener('click', () => {
+                carrito.push(producto)
+                localStorage.setItem("carrito", JSON.stringify(carrito))
+                actualizarCarrito()
+                console.log(carrito)
+            })
+        }
+    })
+}
+
+buscador.addEventListener('keyup', filtrar)
+botonBuscador.addEventListener('click', filtrar)
+
+
+//Función agregar al carrito
 stockDisponible.forEach ((producto) => {
     const botonId = `agregarCarrito${producto.id}`
-    //Creamos el evento click para el botón creado
     document.getElementById(botonId).addEventListener('click', () =>{
-        //Cada vez que hagamos click sobre el botón, agregamos el producto a el array vacío "carrito"
         carrito.push(producto)
-        //Almacenamos el carrito en el LS, pero debemos convertirlo a un string debido que es un objeto, usamos en JSON.stringify
         localStorage.setItem("carrito", JSON.stringify(carrito))
-        //Sacamos el total del carrito mediante el metodo reduce (acumulador, qué vamos a acumular)
-        const totalCarrito = carrito.reduce((acumulador, producto) => acumulador + producto.precio, 0)
-        //Actualizamos el contador del carrito obteniendo el largo del carrito 
-        document.getElementById("count-cart").innerHTML = `${carrito.length} - $${totalCarrito}`
-        //Actualizamos precio al mostrar el carrito y sus productos
-        document.getElementById("foot-modal").innerHTML = `
-    <div>
-        <p style="margin-bottom: 0px; font-weight:700;"> Precio total: $ ${totalCarrito}</p>
-    </div>
-    <div>
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="delete-cart" onclick="vaciarCarrito()">Vaciar carrito</button>
-    </div>
-    `   //Al hacer click sobre el botón creado, ejecutamos la función actualizarCarrito construida abajo
         actualizarCarrito();
     })
 })
 
 const actualizarCarrito = () => {
     //Actualizamos lo que vamos a ver cuando clickeemos sobre el carrito
-    document.getElementById("show-cart").innerHTML = " "
-    carrito.forEach ((prod) => {
-        document.getElementById("show-cart").innerHTML += `<div class="cont-items">
-        <p class="id-show-cart">ID: ${prod.id}</p>
-        <h2 class="nombre-producto-cart">${prod.nombre}</h2>
-        <p class="price-producto-cart">$ ${prod.precio}</p>
-        <button class="btn-eliminar-producto" id="btn-eliminar${prod.id}" onclick="eliminarDelCarrito(${prod.id})"><i class= "fas fa-trash"></i></button>
-        </div>
-        `
-        /*Creamos un nuevo boton que nos permite eliminar el producto que queramos. Recibe por id el nombre que le dimos
-        y le damos la funcion onclick, que al presionar el boton vamos a ejecutar la funcion eliminar del carrito*/
-    })}
-
-//Funcion eliminar carrito que ejecutaremos cada vez que presionemos el boton eliminar incluido al mostrar el carrito
-const eliminarDelCarrito = (productoSeleccionado) => {
-    //Buscamos el producto dentro del carrito que tenga el mismo id que el producto seleccionado a eliminar
-    const producto = carrito.find((prod) => prod.id === productoSeleccionado)
-    //Al encontrarlo, ubicamos el indice del producto mediante el metodo indexOf
-    const indiceProducto = carrito.indexOf(producto)
-    //Y desde su indice, lo eliminamos mediante el metodo splice
-    carrito.splice(indiceProducto, 1)
-    //Actualizamos total del carrito
-    const totalCarrito = carrito.reduce((acumulador, producto) => acumulador + producto.precio, 0)
-    //Actualizamos precio carrito
+    mostradorCarrito.innerHTML = " "
+    let totalCarrito = carrito.reduce((acumulador, producto) => acumulador + producto.precio, 0)
     document.getElementById("foot-modal").innerHTML = `
     <div>
         <p style="margin-bottom: 0px; font-weight:700;"> Precio total: $ ${totalCarrito}</p>
     </div>
     <div>
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="delete-cart" onclick="vaciarCarrito()">Vaciar carrito</button>
+    <button type="button" id="delete-cart" onclick="vaciarCarrito()"><span class="text-vaciar">Vaciar carrito</span></button>
     </div>
     `
-    //Ejecutamos la función actualizar carrito cada vez que eliminemos un producto
-    actualizarCarrito();
-    //Actualizamos contador de carrito
-    document.getElementById("count-cart").innerHTML = `${carrito.length}`
-    console.log(carrito) 
+    console.log(totalCarrito)
+    const contador = document.getElementById("count-cart").innerHTML = `
+        <p>${carrito.length} - $${totalCarrito}</p> `   
+    localStorage.setItem("contador-carrito", JSON.stringify(contador))
+    carrito.forEach ((prod) => {
+        mostradorCarrito.innerHTML += `<div class="cont-items">
+        <img src="${prod.imagen}" class="imagen-cart-producto">
+        <h2 class="nombre-producto-cart">${prod.nombre}</h2>
+        <p class="price-producto-cart">Precio: $${prod.precio}</p>
+        <button class="btn-eliminar-producto" id="btn-eliminar${prod.id}" onclick="eliminarDelCarrito(${prod.id})"><i class= "fas fa-trash"></i></button>
+        </div>
+        `
+        document.getElementById("foot-modal").innerHTML= `
+        <div>
+            <p style="margin-bottom: 0px; font-weight:700;"> Precio total: $ ${totalCarrito}</p>
+        </div>
+        <div>
+            <button type="button" id="delete-cart" onclick="vaciarCarrito()"><span class="text-vaciar">Vaciar carrito</span></button>
+        </div>
+        `
+        localStorage.setItem("mostrador-carrito", JSON.stringify(mostradorCarrito.innerHTML))
+    })
 }
+
+
+const eliminarDelCarrito = (productoSeleccionado) => {
+    const producto = carrito.find((prod) => prod.id === productoSeleccionado)
+    const indiceProducto = carrito.indexOf(producto)
+    carrito.splice(indiceProducto, 1)
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+    actualizarCarrito();
+}
+
+const vaciarCarrito = () => {
+    carrito.length = 0;
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+    actualizarCarrito();
+}
+
+
+//Le damos efecto al header
+
+window.addEventListener('scroll', () => {
+    let header = document.getElementById("cont-header")
+    header.classList.toggle("scroll-abajo", window.scrollY > 0)
+    header.classList.toggle("scroll-arriba", window.scrollY < 0)
+})
+
+
+//Buscar los productos con el buscador
